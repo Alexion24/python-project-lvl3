8ae28f7fd62fe5e892fffb701d9bd34548e9a4fd
Alexion <alex24bryant@mail.ru>
2022-05-26 21:18:17 +0500

    add page-loader logic

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,2 +1,24 @@
-def download():
-    pass
+import os
+import requests
+from urllib.parse import urlparse
+
+
+def download(url, file_path=os.getcwd()):
+    parsed_url = urlparse(url)
+    parsed_path = f'{parsed_url.netloc}{parsed_url.path}'
+    result_string = ''
+    for symbol in parsed_path:
+        if not symbol.isalnum():
+            result_string += '-'
+        else:
+            result_string += symbol
+    result_string += '.html'
+    result_file_path = f'{file_path}/{result_string}'
+    with open(result_file_path, 'w') as file:
+        file.write(get_data_from_url(url))
+    return result_file_path
+
+
+def get_data_from_url(url):
+    response = requests.get(url)
+    return response.text
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,8 +1,19 @@
+import argparse
 from page_loader.page_loader import download
 
 
 def main():
-    download()
+    parser = argparse.ArgumentParser(
+        usage='page-loader [-h] [-o OUTPUT] url',
+        description='Download html page'
+    )
+    parser.add_argument('url')
+    parser.add_argument(
+        '-o', '--output',
+        help='set output directory'
+    )
+    args = parser.parse_args()
+    print(download(args.url, args.output))
 
 
 if __name__ == '__main__':
