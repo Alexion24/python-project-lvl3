fddd291644c4f88c85c6a25782f4011b0da6ccc2
Alexion <alex24bryant@mail.ru>
2022-05-28 22:47:02 +0500

    update fixtures and tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -4,21 +4,21 @@ import tempfile
 from page_loader.page_loader import download
 
 
-HEXLET_URL = 'https://ru.hexlet.io/courses'
-RU_HEXLET_IO_COURSES_HTML = os.path.join(
-    'tests', 'fixtures', 'ru-hexlet-io-courses.html'
+HEXLET_URL = 'https://page-loader.hexlet.repl.co'
+HTML_FIXTURE = os.path.join(
+    'tests', 'fixtures', 'page-loader-hexlet-repl-co.html'
 )
 
 
 def read_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
 
 def test_download(requests_mock):
     with tempfile.TemporaryDirectory() as tmpdir:
-        file_data = read_file(RU_HEXLET_IO_COURSES_HTML)
+        file_data = read_file(HTML_FIXTURE)
         requests_mock.get(HEXLET_URL, text=file_data)
         assert download(HEXLET_URL, tmpdir) == \
-               os.path.join(tmpdir, 'ru-hexlet-io-courses.html')
+               os.path.join(tmpdir, 'page-loader-hexlet-repl-co.html')
         assert file_data == requests.get(HEXLET_URL).text
