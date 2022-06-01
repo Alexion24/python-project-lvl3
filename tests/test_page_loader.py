ce66e36bfbb8c8e4495198b34cf31d77512558d3
Alexion <alex24bryant@mail.ru>
2022-06-01 20:43:45 +0500

    add tests from step 3

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -3,20 +3,35 @@ import tempfile
 from page_loader.page_loader import download
 
 
-URL = 'https://page-loader.hexlet.repl.co'
-IMAGE_URL = 'https://page-loader.hexlet.repl.co/assets/professions/nodejs.png'
-HTML_PAGE_NAME = 'page-loader-hexlet-repl-co.html'
-ORIGINAL_PAGE = os.path.join(
+URL = 'https://ru.hexlet.io/courses'
+IMAGE_URL = 'https://ru.hexlet.io/assets/professions/nodejs.png'
+CSS_URL = 'https://ru.hexlet.io/assets/application.css'
+SCRIPT_URL = 'https://ru.hexlet.io/packs/js/runtime.js'
+HTML_PAGE_NAME = 'ru-hexlet-io-courses.html'
+INNER_HTML_PAGE_NAME = 'ru-hexlet-io-courses.html'
+ORIGINAL_PAGE_FIXTURE = os.path.join(
     'tests', 'fixtures', 'original_page.html'
 )
-DOWNLOADED_PAGE = os.path.join(
+DOWNLOADED_PAGE_FIXTURE = os.path.join(
     'tests', 'fixtures', 'downloaded_page.html'
+)
+INNER_HTML_FIXTURE = os.path.join(
+    'tests', 'fixtures', 'inner_html.html'
 )
 IMAGE_FIXTURE = os.path.join(
     'tests', 'fixtures', 'nodejs.png'
 )
-IMAGE_PATH = 'page-loader-hexlet-repl-co_files/' \
-             'page-loader-hexlet-repl-co-assets-professions-nodejs.png'
+CSS_FIXTURE = os.path.join(
+    'tests', 'fixtures', 'css_file.css'
+)
+SCRIPT_FIXTURE = os.path.join(
+    'tests', 'fixtures', 'script.js'
+)
+IMAGE_PATH = \
+    'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png'
+INNER_HTML_PATH = 'ru-hexlet-io-courses_files/ru-hexlet-io-courses.html'
+CSS_PATH = 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-application.css'
+SCRIPT_PATH = 'ru-hexlet-io-courses_files/ru-hexlet-io-packs-js-runtime.js'
 
 
 def read_file(file_path, mode='r'):
@@ -26,10 +41,19 @@ def read_file(file_path, mode='r'):
 
 def test_download(requests_mock):
     with tempfile.TemporaryDirectory() as tmpdir:
-        requests_mock.get(URL, text=read_file(ORIGINAL_PAGE))
+        requests_mock.get(URL, text=read_file(ORIGINAL_PAGE_FIXTURE))
         requests_mock.get(IMAGE_URL, content=read_file(IMAGE_FIXTURE, 'rb'))
+        requests_mock.get(URL, text=read_file(INNER_HTML_FIXTURE))
+        requests_mock.get(CSS_URL, text=read_file(CSS_FIXTURE))
+        requests_mock.get(SCRIPT_URL, text=read_file(SCRIPT_FIXTURE))
         download(URL, tmpdir)
-        result_file = os.path.join(tmpdir, HTML_PAGE_NAME)
+        result_html = os.path.join(tmpdir, HTML_PAGE_NAME)
         result_png = os.path.join(tmpdir, IMAGE_PATH)
-        assert read_file(result_file) == read_file(DOWNLOADED_PAGE)
+        result_inner_html = os.path.join(tmpdir, INNER_HTML_PATH)
+        result_css = os.path.join(tmpdir, CSS_PATH)
+        result_script = os.path.join(tmpdir, SCRIPT_PATH)
+        assert read_file(result_html) == read_file(DOWNLOADED_PAGE_FIXTURE)
         assert read_file(result_png, 'rb') == read_file(IMAGE_FIXTURE, 'rb')
+        assert read_file(result_inner_html) == read_file(INNER_HTML_FIXTURE)
+        assert read_file(result_css) == read_file(CSS_FIXTURE)
+        assert read_file(result_script) == read_file(SCRIPT_FIXTURE)
