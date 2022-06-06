9a9f90d9a111160f91c6c90cc03fdf7d626cdc54
Alexion <alex24bryant@mail.ru>
2022-06-06 16:08:34 +0500

    add tests with errors

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,7 +1,9 @@
 import os
 import tempfile
+import pytest
 from page_loader.page_loader import download
-
+from page_loader.data_handler import get_data_from_url, save_data_to_file
+from urllib.parse import urljoin
 
 URL = 'https://ru.hexlet.io/courses'
 IMAGE_URL = 'https://ru.hexlet.io/assets/professions/nodejs.png'
@@ -57,3 +59,25 @@ def test_download(requests_mock):
         assert read_file(result_inner_html) == read_file(INNER_HTML_FIXTURE)
         assert read_file(result_css) == read_file(CSS_FIXTURE)
         assert read_file(result_script) == read_file(SCRIPT_FIXTURE)
+
+
+def test_response_with_error(requests_mock):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        url1 = urljoin(URL, '403')
+        url2 = urljoin(URL, '404')
+        url3 = urljoin(URL, '500')
+        requests_mock.get(url1, status_code=403)
+        requests_mock.get(url2, status_code=404)
+        requests_mock.get(url3, status_code=500)
+        with pytest.raises(Exception):
+            assert download(url1, tmpdir)
+            assert download(url2, tmpdir)
+            assert download(url3, tmpdir)
+
+
+def test_save_to_file_with_permission_error(requests_mock):
+    requests_mock.get(URL, text=read_file(ORIGINAL_PAGE_FIXTURE))
+    data_from_url = get_data_from_url(URL)
+    file_path = '/random_file_path'
+    with pytest.raises(PermissionError):
+        save_data_to_file(file_path, data_from_url)
